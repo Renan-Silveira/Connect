3fe09edcddc09b152e16f3a43bc7947e7af9aae3
Renan Silveira <renanesilveira@hotmail.com>
2026-05-21 21:06:46 -0300

    Create flow.py
    criando o arquivo flow para a pipeline

---
+++ src/flow.py
@@ -0,0 +1,39 @@
+def main():
+    from extract import extract
+    from rename_pkl import rename_parquet
+    from rename_csv import rename_csv
+    import logging
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    logger = logging.getLogger(__name__)
+    logger.info("Pipeline iniciada")
+
+    steps = [
+        extract,
+        rename_csv,
+        rename_parquet
+    ]
+
+    for step in steps:
+        logger.info(f"Executando etapa: {step.__name__}") # Loga o início de cada etapa
+
+        try:
+            result = step() # Executa a etapa e captura o resultado
+
+            if not result:
+                logger.error(f"Etapa falhou: {step.__name__}") # Loga o erro e
+                break
+
+            logger.info(f"Etapa concluída: {step.__name__}") # Loga o sucesso da etapa
+
+        except Exception as e:
+            logger.exception(f"Erro inesperado em {step.__name__}: {e}") # Loga a exceção e
+            break
+
+    logger.info("Pipeline finalizada")
+if __name__ == "__main__":
+    main()
