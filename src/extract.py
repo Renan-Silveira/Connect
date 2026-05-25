f963d28071d717d9699fcb61611ba68e0cdd0c2b
Renan Silveira <renanesilveira@hotmail.com>
2026-05-25 20:23:34 -0300

    Update extract.py
    update da documentação

--- src/extract.py
+++ src/extract.py
@@ -1,22 +1,18 @@
-from sqlalchemy import true
-
-
 def extract():
     import requests # Biblioteca para fazer requisições HTTP
     import os # Biblioteca para interagir com o sistema operacional
     from dotenv import load_dotenv # Biblioteca para carregar variáveis de ambiente a partir de um arquivo .env
     import zipfile # Biblioteca para manipular arquivos ZIP
     from tqdm import tqdm # Biblioteca para criar barras de progresso no terminal
-    import requests # Biblioteca para fazer requisições HTTP
     import logging # Biblioteca para registrar logs de erros
     from datetime import datetime # Biblioteca para trabalhar com datas e horas
 
     load_dotenv() # Carrega as variáveis de ambiente do arquivo .env
 
-    URL = os.getenv("URL") # Obtém a URL do arquivo a ser baixado a partir das variáveis de ambiente
-    DIR_DATA = os.getenv("DIR_DATA") # Obtém o diretório onde o arquivo será salvo a partir das variáveis de ambiente
-    DIR_DATARAW = os.getenv("DIR_DATARAW") # Obtém o diretório onde os dados extraídos serão salvos a partir das variáveis de ambiente
-    NOME_FILE = os.getenv("NOME_FILE") # Obtém o nome do arquivo a ser salvo a partir das variáveis de ambiente
+    URL = os.getenv("URL") # Obtém a URL do arquivo
+    DIR_DATA = os.getenv("DIR_DATA") # Obtém o diretório onde o arquivo será salvo
+    DIR_DATARAW = os.getenv("DIR_DATARAW") # Obtém o diretório onde os dados extraídos serão salvos
+    NOME_FILE = os.getenv("NOME_FILE") # Obtém o nome do arquivo a ser salvo
 
     # Configuração do log: salva em 'erros_download.txt', modo 'a' (append)
     logging.basicConfig(
@@ -25,7 +21,7 @@ def extract():
         format='%(asctime)s - %(levelname)s - %(message)s'
     )
 
-    def download_file(URL, path): # Função para baixar um arquivo da URL especificada e salvar no caminho fornecido
+    def download_file(URL, path): # Função para baixar um arquivo da URL
         chunk_size = 1024 * 1024  # Aumenta o tamanho do chunk para 1MB (1024*1024) para reduzir chamadas de sistema
         
         try:
@@ -46,7 +42,7 @@ def extract():
         except requests.exceptions.RequestException as e: # Captura erros relacionados a requisições HTTP
             print(f"\nErro ao baixar o arquivo: {e}") # Imprime uma mensagem de erro caso ocorra um problema durante o download
     try:
-        #download_file(URL, f"{DIR_DATA}/{NOME_FILE}") # Chama a função para baixar o arquivo
+        download_file(URL, f"{DIR_DATA}/{NOME_FILE}") # Chama a função para baixar o arquivo
 
         with zipfile.ZipFile(f"{DIR_DATA}/{NOME_FILE}", "r") as zip_ref: # Abre o arquivo ZIP para leitura
             zip_ref.extractall(DIR_DATARAW) # Extrai todo o conteúdo do arquivo ZIP para o diretório especificado
